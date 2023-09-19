c4c17ab1af3039817538ac3d2c046be4d8c036f9
Agustin Sanchez <pagustinsanchez17@gmail.com>
2023-09-19 09:58:38 -0300

    db raw data

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,37 @@
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+
+
 from app import views
+from .db import fill_db
+
+
+
+def fill_db_view(request):
+    result = fill_db()  # Llama a la función fill_db
+    if result:
+            return JsonResponse({'message': 'Datos cargados con éxito'}, status=201)
+    else:
+        return JsonResponse({'error': 'Hubo un problema al cargar los datos'}, status=500)
+
+urlpatterns = [
+    path('api/v1/', include([
+        path('barrios/', views.BarriosView.as_view({'get': 'list', 'post': 'create'}), name='barrios-list'),
+        path('campus/', views.CampusView.as_view({'get': 'list', 'post': 'create'}), name='campus-list'),
+        path('carreras/', views.CarrerasView.as_view({'get': 'list', 'post': 'create'}), name='carreras-list'),
+        # ... otras rutas para tus viewsets ...
+
+        # Agrega la ruta para llamar a la función fill_db_view
+        path('filldb/', fill_db_view, name='filldb'),
+    ])),
+    path('docs/', include_docs_urls(title='App API')),
+]
+
+'''
 
 router = routers.DefaultRouter()
 router.register(r'barrios', views.BarriosView, 'barrios')
@@ -18,8 +48,11 @@ router.register(r'provincias', views.ProvinciasView, 'provincias')
 router.register(r'tiposPersona', views.TipoPersonaView, 'tiposPersona')
 router.register(r'titulaciones', views.TitulacionesView, 'titulaciones')
 router.register(r'universidades', views.universidadesView, 'universidades')
+router.register(r'filldb', fill_db_view, 'filldb')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('docs/', include_docs_urls(title='App API'))
 ]
+
+'''
