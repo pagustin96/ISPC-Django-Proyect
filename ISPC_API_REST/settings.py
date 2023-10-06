14aec9aa78cd8aeca8a0d453ec18a094ab7c7ea1
Julian <105244530+Julianfried@users.noreply.github.com>
2023-10-06 20:48:23 -0300

    Update settings.py

--- ISPC_API_REST/settings.py
+++ ISPC_API_REST/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ispc',
         'USER': 'root',
-        'PASSWORD': 'jqkre2__',
+        'PASSWORD': 'root',
         'HOST': 'localhost',  # O la dirección de tu servidor MySQL
         'PORT': '3306',       # El puerto predeterminado de MySQL es 3306
     }
